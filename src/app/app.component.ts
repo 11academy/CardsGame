c956519c27f252844cc343300db340ba617cc853
Bione <fabio.pergamiferoldi@gmail.com>
2019-07-23 16:49:24 +0200

    Risolto conflitto del merge

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Component } from '@angular/core';
 
 @Component({
@@ -9,17 +8,3 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'CardsGame';
 }
-=======
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'CardsGame';
-
-
-}
->>>>>>> boxCardM
