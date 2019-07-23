010aa8640e3d140278b3f7fceecf96a383671ea5
Fedora <monica.fiore8519@gmail.com>
2019-07-23 10:52:41 +0200

    Dare un voto e dare più voti

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BoxCardComponent } from './box-card/box-card.component';
+import { SingleCardComponent } from './box-card/single-card/single-card.component';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
+const rotte: Routes = [
+  {path: '', component:AppComponent},
+  {path:'card', component:BoxCardComponent},
+  {path:'single-card/:num', component:SingleCardComponent}
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoxCardComponent,
+    SingleCardComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(rotte)
+
   ],
+ 
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/box-card/box-card.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, Input, Output } from "@angular/core"
+import { ActivatedRoute } from "@angular/router"
+
+@Component({
+	selector: "app-box-card",
+	templateUrl: "./box-card.component.html",
+	styleUrls: ["./box-card.component.css"]
+})
+export class BoxCardComponent implements OnInit {
+	card_number = [1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+
+
+
+	num: number;
+	
+
+
+	getCurrentCard(num) {
+		this.num = num
+		console.log(this.num)
+	}
+
+	constructor(private route: ActivatedRoute) {}
+
+	ngOnInit() {
+	
+		//this.route.params.subscribe((params) => this.myValue = params.myValue)
+	}
+}
